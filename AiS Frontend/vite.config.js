b96acca9e06b8388d053633b56c4c291d7b491ae
shubh <shubham.dhanawade@prosoftapp.com>
2025-07-11 00:30:30 +0530

    hii

--- AiS Frontend/vite.config.js
+++ AiS Frontend/vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import Pages from 'vite-plugin-pages';
+import Sitemap from 'vite-plugin-sitemap';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss() ,  react(),
+    Pages(),
+    Sitemap({
+      hostname: 'https://appliedinsights.in',
+    }),],
 });
