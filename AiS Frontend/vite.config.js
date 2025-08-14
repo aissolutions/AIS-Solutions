a5e9b0f5a19c3b5f2909c719d3fb8c5451a6dfea
Krushna Kharat <krushnakharat.official@gmail.com>
2025-08-14 14:12:15 +0530

    another

--- AiS Frontend/vite.config.js
+++ AiS Frontend/vite.config.js
@@ -1,14 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import Pages from 'vite-plugin-pages';
-import Sitemap from 'vite-plugin-sitemap';
+import Pages from "vite-plugin-pages";
+import Sitemap from "vite-plugin-sitemap";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss() ,  react(),
+  plugins: [
+    react(),
+    tailwindcss(),
     Pages(),
     Sitemap({
-      hostname: 'https://appliedinsights.in',
-    }),],
+      hostname: "https://appliedinsights.in",
+    }),
+  ],
 });
